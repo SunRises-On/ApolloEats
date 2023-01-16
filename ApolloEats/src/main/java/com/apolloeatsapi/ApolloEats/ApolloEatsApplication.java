4c4ab513a3eee90850a539fda165fe5439460b23
Jewelle Wilson <jewellelenicewilson@gmail.com>
2023-01-16 14:20:16 -0600

    Add backend.

---
+++ ApolloEats/src/main/java/com/apolloeatsapi/ApolloEats/ApolloEatsApplication.java
@@ -0,0 +1,13 @@
+package com.apolloeatsapi.ApolloEats;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApolloEatsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApolloEatsApplication.class, args);
+	}
+
+}
---
+++ ApolloEats/src/test/java/com/apolloeatsapi/ApolloEats/ApolloEatsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.apolloeatsapi.ApolloEats;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApolloEatsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
